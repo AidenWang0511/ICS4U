282e80ceb7b60d37edf6914beedf9f66383c5597
AidenWang0511 <70038087+AidenWang0511@users.noreply.github.com>
2021-09-27 23:43:43 -0400

    java practises

--- IOReview.java
+++ IOReview.java
@@ -9,13 +9,73 @@ import java.io.*;
 
 public class IOReview {
     public static void main(String[] args) throws Exception{
+
+        //quesiton 1
         File myFile = new File("writeEx1");
         PrintWriter output = new PrintWriter(myFile);
-        //Scanner in = new Scanner(myFile);
         String s = "POPCORN";
         for(int i = 0; i < s.length(); i++){
             output.println(s.charAt(i));
         }
         output.close();
+
+
+        //quesiton 2
+        Scanner myScanner = new Scanner(System.in);
+        int[] marks = new int[5];
+        //prompt user for 5 marks
+        for(int i=0; i<5; i++){
+            System.out.println("Please enter a integer mark between 0-100");
+            marks[i] = myScanner.nextInt();
+        }
+        //bubble sort marks
+        for(int i=4; i>0; i--){
+            for(int j=0; j<i; j++){
+                if(marks[j] > marks[j+1]){
+                    int temp = marks[j];
+                    marks[j] = marks[j+1];
+                    marks[j+1] = temp;
+                }
+            }
+        }
+        myScanner.nextLine();//clear the scanner
+        System.out.println("File name you want to print these marks to: ");//prompt user for file name
+        String fileName = myScanner.nextLine();
+        File markFile = new File(fileName);//create the file
+        output = new PrintWriter(markFile);//create owriter for the file
+        for(int i=0; i<5; i++){
+            output.println(marks[i]);//print marks on to file
+        }
+        output.close();//close the printer to save
+
+
+        //question 3
+        Scanner in = new Scanner(myFile);//create scanner for file in quesiton 1
+        while(in.hasNext()){//scan the file and print whatever is in it to the console
+            s = in.next();
+            System.out.println(s);
+        }
+        in.close();
+
+
+        //question 4
+        in = new Scanner(markFile);//create scanner for file in quesiton 2
+        int avg = 0;
+        int passed = 0;
+        int failed = 0;
+        while(in.hasNext()){//scan the file and print whatever is in it to the console
+            int mark = in.nextInt();
+            if(mark > 49){
+                passed++;
+            }else{
+                failed++;
+            }   
+            avg += mark;
+        }
+        avg = avg/5;//calculates the average
+        //print as required
+        System.out.println("You have passed " + passed + " tests and failed " + failed + " tests.");
+        System.out.println("The average mark of the tests was " + avg + ".");
+        in.close();
     }
 }
--- JavaBasicReview.java
+++ JavaBasicReview.java
@@ -1,6 +1,11 @@
+/**
+* Name: Aiden Wang
+* Date: Sep 17, 2021
+* Description: java basic review questions
+*/
 import java.util.*;
 
-public class Review1{
+public class JavaBasicReview{
   public static void main(String[] args) {
     Scanner myScanner = new Scanner(System.in);
     
@@ -35,6 +40,38 @@ public class Review1{
       check = myScanner.next();
       
     }
+
+    myScanner.nextLine();//clear the scanner
+
+    //question 2
+    System.out.println("Please enter a String: ");
+    String user = myScanner.nextLine();
+    do{//keep printing the String user entered until user says no
+      System.out.println(user);
+      System.out.println("do you want to keep printing(yes/no)");
+      check = myScanner.next();
+    }while(!check.equals("no"));
+
+    //question 3
+    System.out.println("You have 3 chance to guess a number between 1-10 inclusive!");
+    int num = (int)(Math.random()*10) + 1;//generate a number from 1 to 10 inclusive
+    //for loop 3 times for user to guess, if correct, break the for loop
+    for(int i=3; i>=1; i--){
+      System.out.println("Please input your guess: (" + i + " chances remaining)");
+      int guess = myScanner.nextInt();
+      if(guess == num){
+        System.out.println("Correct! :) It took you " + (4-i) + " times to guess it right!");
+        num = -1;//set num to -1 for checking purposes 
+        break;
+      }else{
+        System.out.println("Incorrect! :(");
+      }
+    }
+    //if gueesed correctly within 3 tries, won't print the correct answer
+    if(num != -1){
+      System.out.println("The correct answer is: " + num);
+    }
+
     
   }
 }
