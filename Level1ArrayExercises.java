9c868cbc46f207159a055cb1188180c83e4e28cd
AidenWang0511 <aidenwang0511@gmail.com>
2021-09-29 20:52:49 -0400

    Update Level1ArrayExercises.java

--- Level1ArrayExercises.java
+++ Level1ArrayExercises.java
@@ -44,12 +44,20 @@ public class Level1ArrayExercises {
      * @return void - procedure method
      */
     public static void enterFromKeyboard(int[] arr){
-        Scanner in = new Scanner(System.in);
-        //prompt user for 10 integers
-        for(int i=0; i<arr.length; i++){
-            System.out.println("Please enter an integer: ");
-            arr[i] = in.nextInt(); 
+        Scanner in = new Scanner(System.in); //Scanner for input
+        int counter = 0; //counter for up to 10 int
+        while(counter < 10){ //make sure only 10 integers can be entered
+            System.out.println("Do you want to enter a integer: (yes/no)"); //ask the user to contiue enter number or not
+            String check = in.next();
+            if(check.equals("yes")){//if user want to enter a number, then prompt the user and increase counter by 1
+                System.out.println("Please enter an integer: ");
+                arr[counter] = in.nextInt();
+                counter++;
+            }else{//otherwise, the moethod ends
+                return;
+            }
         }
+        //when the while loop condition is false, it mean the user has entered 10 integers already
+        System.out.println("Only 10 integers can be entered!");
     }
-    
 }
