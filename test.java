52f7fa153be3d4effe3d27df4000b56b2d5946ee
AidenWang0511 <aidenwang0511@gmail.com>
2021-10-26 14:20:19 -0400

    good stuff

--- test.java
+++ test.java
@@ -5,5 +5,22 @@ public class test{
         String a = "ABCD";
         String b = a.substring(4);
         System.out.println(b);
+        
     }
+
+    /**
+     * Euclid's algorithm for determining the greatest common divisor
+     * Method name: gcd
+     * @param m - an integer to be used
+     * @param n - an integer to be used
+     * @return the greatest common divisor of m and n
+     */
+    public static int gcd(int m, int n) {
+        // base case
+        if (m == 0) {
+            return n;
+        }
+        return gcd(n % m, m);
+    }
+
 }
