b899593d992f2833dadced88425f84d03be34334
AidenWang0511 <70038087+AidenWang0511@users.noreply.github.com>
2021-12-07 03:07:17 -0500

    quicksort for james part presentation

--- QuickSort.java
+++ QuickSort.java
@@ -1,39 +1,39 @@
 import java.util.*;
 public class QuickSort {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        
+        String[] array = {"j", "a", "d", "r", "p"}; //array of names
+        quickSort(array, 0, array.length - 1);  //call the sort method
+        for (int i = 0; i < array.length; i++) { //pring the array
+            System.out.println(array[i]);
+        }
+    }
+    
+    //quick sort recursive method
+    public static void quickSort(String[] array, int low, int high) {
+        if (low < high) { //base case
+            int pivot = partition(array, low, high); //get the pivot
+            quickSort(array, low, pivot - 1); //sort the low side
+            quickSort(array, pivot + 1, high); //sort the high side
+        }
+    }
 
-        //sort string array using quick sort in alphabetical order
-        String[] strArray = {"pqr", "stu", "vwx", "abc","def", "ghi", "jkl", "mno", "yz"};    
-        quickSort(strArray, 0, strArray.length-1);
-        System.out.println("Sorted String Array: ");
-        for(String s: strArray){
-            System.out.print(s + " ");
-        }
-    }
-    //quick sort method
-    public static void quickSort(String[] strArray, int low, int high){
-        if(low < high){
-            int pivot = partition(strArray, low, high);
-            quickSort(strArray, low, pivot-1);
-            quickSort(strArray, pivot+1, high);
-        }
-    }
-    //partition method
-    public static int partition(String[] strArray, int low, int high){
-        String pivot = strArray[high];
-        int i = low-1;
-        for(int j = low; j < high; j++){
-            if(strArray[j].compareTo(pivot) > 0){
-                i++;
-                String temp = strArray[i];
-                strArray[i] = strArray[j];
-                strArray[j] = temp;
+    //partition the array
+    public static int partition(String[] array, int low, int high) {
+        String pivot = array[high]; //get the pivot
+        int i = low - 1; //index of smaller element
+        for(int j = low; j<high; j++){ //loop through array
+            if(array[j].compareTo(pivot) < 0){ //if element is smaller than pivot
+                i++; //increment index of smaller element
+                String temp = array[i]; //swap elements
+                array[i] = array[j]; 
+                array[j] = temp;
             }
         }
-        String temp = strArray[i+1];
-        strArray[i+1] = strArray[high];
-        strArray[high] = temp;
-        return i+1;
+        String temp = array[i+1]; //swap elements 
+        array[i+1] = array[high]; 
+        array[high] = temp; 
+        return i+1; //return index of pivot
     }
    
 }
