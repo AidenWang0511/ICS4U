15c845ec7a1f2320f16f803409ee569c78df8b4a
AidenWang0511 <aidenwang0511@gmail.com>
2021-12-09 13:25:32 -0500

    Create SequentialSearch.java

---
+++ SequentialSearch.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class SequentialSearch {
+    public static void main(String[] args) {
+        //sequential search algorithm
+        //for loop through the array from index 0 to less than the length of the array
+            //if the value at the current index is equal to the value we are looking for
+                //print the index
+    }
+}
