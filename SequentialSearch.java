07ea8972f0eeca6018db5a4d4effa677495f98c8
AidenWang0511 <aidenwang0511@gmail.com>
2021-12-15 13:04:44 -0500

    pdfs and stuff

--- SequentialSearch.java
+++ SequentialSearch.java
@@ -2,13 +2,48 @@ import java.util.*;
 
 public class SequentialSearch {
     public static void main(String[] args) {
-        //sequential search algorithm
-        //create a flag boolean to check if the element is found
-        //for loop through the array from index 0 to less than the length of the array
-            //if the value at the current index is equal to the value we are looking for
-                //print the index
-                //set the flag to true
-        //if the flag is false
-            //print that the value was not found
+        
     }
+    /**
+     * Binary Search a number in an sorted array
+     * method name: binarySearch
+     * @param arr the array to search
+     * @param value the value to search for
+     * @return the index of the value if found, -1 if not found
+     */
+    public static int binarySearch(int[] arr, int value) {
+        //create left and right indexes;
+        //left index is 0, right index is the length of the array - 1
+        //while the left <= right
+            //set mid to the average of left and right (mid = (left+right) / 2))
+            //if arr[mid] == value
+                //return the mid index
+            //if arr[mid] > value
+                //set the right index to mid-1
+            //if arr[mid] < value
+                //set the left index to mid+1
+        return -1;
+    }
+
+    /**
+     * Recursive binary search
+     * method name: recrusiveBinarySearch
+     * @param arr the array to search
+     * @param value the value to search for
+     * @param left the left index
+     * @param right the right index
+     * @return the index of the value if found, -1 if not found
+     */
+    public static int recursiveBinarySearch(int[] arr, int value, int left, int right) {
+        //if left > right
+            //return -1
+        //set mid to the average of left and right (mid = (left+right) / 2))
+        //if arr[mid] == value
+            //return the mid index
+        //if value < arr[mid]
+            //return recursiveBinarySearch(arr, value, left, mid-1)
+        //else 
+            //return recursiveBinarySearch(arr, value, mid+1, right)
+    }
+
 }
